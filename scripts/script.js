6b85a3b69d6a22bd7ca4c1835329b948c38644ba
Jon Moon <44587416+jonjhmoon@users.noreply.github.com>
2021-02-13 15:38:33 -0800

    script q4

--- scripts/script.js
+++ scripts/script.js
@@ -9,9 +9,9 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   else { // add it to local storage
     fetch('https://fakestoreapi.com/products')
-      .then((response) => response.json())
-      .then((data) => JSON.stringify(data))
-      .then((info) => localStorage.getItem('products'))
+      .then( (response) => response.json())
+      .then( (data) => JSON.stringify(data))
+      .then( (info) => localStorage.getItem('products'))
       .then( () => {
         prods = JSON.parse(localStorage.getItem('products'));
       })
@@ -21,3 +21,23 @@ window.addEventListener('DOMContentLoaded', () => {
       });
   }
 });
+
+let loadProd = function() {
+  let cart = localStorage.getItem('cart').split(',');
+  let list = document.createElement('product-item');
+  list.setAttribute('id', item.id);
+  document.getElementById('product-list').appendChild(listing);
+  list.shadowRoot.querySelector('.title').innerHTML = item.title;
+  list.shadowRoot.querySelector('.price').innerHTML = '$' + item.price.toFixed(2);
+  list.shadowRoot.querySelector('img').src = item.image;
+  list.shadowRoot.querySelector('img').src = item.image;
+  list.shadowRoot.querySelector('img').alt = item.title;
+
+  if (cart[item.id - 1] === '0') {
+    list.shadowRoot.querySelector('btn').innerHTML = 'Add to cart';
+  } else {
+    list.shadowRoot.querySelector('btn').innerHTML = 'Remove from cart';
+    document.getElementById('cart-count').innerHTML++;
+  }
+  return item;
+};
