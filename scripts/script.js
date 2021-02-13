fffbbd8c609f54533f941820e4131929d52dca2b
Jon Moon <44587416+jonjhmoon@users.noreply.github.com>
2021-02-13 15:23:53 -0800

    script st1

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,23 @@
-// Script.js
-
 // create a fetch request to the following URL:
 window.addEventListener('DOMContentLoaded', () => {
+  let prods = JSON.parse(localStorage.getItem('products'));
+
   // check before your initial fetch request now to see if it is in local storage first
-  if('products' in localStorage){
-    alert('in localStorage');
-    // alert(localStorage.getItem('products'[0])); 
-  } 
+  if(prods in localStorage){
+    for (const i in prods)
+      loadProd(prods[i]);
+  }
   else { // add it to local storage
-    // alert('no');
-    const myURL = fetch('https://fakestoreapi.com/products');
-    localStorage.setItem('products', myURL);
+    fetch('https://fakestoreapi.com/products')
+      .then((response) => response.json())
+      .then((data) => JSON.stringify(data))
+      .then((info) => localStorage.getItem('products'))
+      .then( () => {
+        prods = JSON.parse(localStorage.getItem('products'));
+      })
+      .then( () =>{
+        localStorage.setItem('cart', new Array(products.length).fill(0).toString());
+        for (const i in prods) loadProd(prods[i]);
+      });
   }
 });
