2c54ac9f0bb0c729a08406a4753cbeb031bd774e
arelae <arelae25@gmail.com>
2021-02-13 14:14:33 -0800

    updated

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,15 @@
 // Script.js
 
+// create a fetch request to the following URL:
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  // check before your initial fetch request now to see if it is in local storage first
+  if('products' in localStorage){
+    alert('in localStorage');
+    // alert(localStorage.getItem('products'[0])); 
+  } 
+  else { // add it to local storage
+    // alert('no');
+    const myURL = fetch('https://fakestoreapi.com/products');
+    localStorage.setItem('products', myURL);
+  }
 });
