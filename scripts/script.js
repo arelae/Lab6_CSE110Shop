9800a300017aebf8d91d060bf5bb53652559b977
Jon Moon <44587416+jonjhmoon@users.noreply.github.com>
2021-02-13 16:51:49 -0800

    script

--- scripts/script.js
+++ scripts/script.js
@@ -1,47 +1,43 @@
 // create a fetch request to the following URL:
-window.addEventListener('DOMContentLoaded', () => {
-  let prods = JSON.parse(localStorage.getItem('products'));
+window.addEventListener("DOMContentLoaded", () => {
+  let prods = JSON.parse(localStorage.getItem("products"));
 
-  // check before your initial fetch request now to see if it is in local storage first
-  if(prods in localStorage){
-    for (const i in prods)
-      loadProd(prods[i]);
-  }
-  else { // add it to local storage
-    fetch('https://fakestoreapi.com/products')
+  if (prods === null) {
+    console.log("initial fetch");
+    fetch("https://fakestoreapi.com/products")
       .then( (response) => response.json())
       .then( (data) => JSON.stringify(data))
-      .then( (info) => localStorage.getItem('products'))
+      .then( (info) => localStorage.setItem("products", info))
       .then( () => {
-        prods = JSON.parse(localStorage.getItem('products'));
+        prods = JSON.parse(localStorage.getItem("products"));
       })
-      .then( () =>{
-        localStorage.setItem('cart', new Array(products.length).fill(0).toString());
-        for (const i in prods) loadProd(prods[i]);
+      .then(() => {
+        localStorage.setItem(
+          "cart",
+          new Array(prods.length).fill(0).toString()
+        );
+        for (const item in prods) loadProduct(prods[item]);
       });
+  } else {
+    for (const item in prods) loadProduct(prods[item]);
   }
 });
 
-let loadProd = (item) = function(){myFunction()};
-    function myFunction(){
-      let cart = localStorage.getItem('cart').split(',');
-      let list = document.createElement('product-item');
-      
-      list.setAttribute('id', item.id);
-      
-      document.getElementById('product-list').appendChild(listing);
+let loadProduct = (item) => {
+  let cart = localStorage.getItem("cart").split(",");
+  let list = document.createElement("product-item");
+  list.setAttribute("id", item.id);
+  document.getElementById("product-list").appendChild(list);
+  list.shadowRoot.querySelector(".title").innerHTML = item.title;
+  list.shadowRoot.querySelector(".price").innerHTML = "$" + item.price.toFixed(2);
+  list.shadowRoot.querySelector("img").src = item.image;
+  list.shadowRoot.querySelector("img").src = item.image;
+  list.shadowRoot.querySelector("img").alt = item.title;
 
-      list.shadowRoot.querySelector('.title').innerHTML = item.title;
-      list.shadowRoot.querySelector('.price').innerHTML = '$' + item.price.toFixed(2);
-      list.shadowRoot.querySelector('img').src = item.image;
-      list.shadowRoot.querySelector('img').src = item.image;
-      list.shadowRoot.querySelector('img').alt = item.title;
-
-      if (cart[item.id - 1] === '0') {
-        list.shadowRoot.querySelector('btn').innerHTML = 'Add to cart';
-      } 
-      else {
-        list.shadowRoot.querySelector('btn').innerHTML = 'Remove from cart';
-        document.getElementById('cart-count').innerHTML++;
-      }
-    }
+  if (cart[item.id - 1] === "0") {
+    list.shadowRoot.querySelector("button").innerHTML = "Add to Cart";
+  } else {
+    list.shadowRoot.querySelector("button").innerHTML = "Remove from Cart";
+    document.getElementById("cart-count").innerHTML++;
+  }
+};
