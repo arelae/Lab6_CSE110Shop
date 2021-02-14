592f505f74d3e27dc02de126dba1cf21ad2130eb
Jon Moon <44587416+jonjhmoon@users.noreply.github.com>
2021-02-13 16:48:39 -0800

    product

--- components/product-item.js
+++ components/product-item.js
@@ -1,80 +1,27 @@
 // product-item.js
 
 class ProductItem extends HTMLElement {
+  // TODO
   constructor() {
     super();
     let shadow = this.attachShadow({mode: "open"});
     let image = document.createElement("img");
     let title = document.createElement("p");
     let price = document.createElement("p");
-    let btn = document.createElement("button");
-
-    title.setAttribute("class", "title");
-    price.setAttribute("class", "price");
-
-    btn.innerHTML = "Add to Cart";
-    btn.onclick = function() {myFunction()};
-    function myFunction() {
-      let myCart = localStorage.getItem("cart").split(",");
-      if (myCart[this.id - 1] == "0") { 
-        myCart[this.id - 1] = "1";
-        localStorage.setItem("cart", myCart);
-        btn.innerHTML = "Remove from Cart";
-        document.getElementById("cart-count").innerHTML++;
-      } 
-      else{
-        myCart[this.id - 1] = "0";
-        localStorage.setItem("cart", myCart);
-        btn.innerHTML = "Add to Cart";
-        document.getElementById("cart-count").innerHTML--;
-      }
-    }
-
-    let myList = document.createElement("li");
-    myList.setAttribute("class", "product");
-
-    const style = document.createElement("link");
-
-    style.setAttribute('rel', 'spreadsheet');
-    style.setAttribute('href', 'styles/styles.css');
-
-    shadow.appendChild(style);
-    shadow.appendChild(item);
-
-    myList.appendChild(image);
-    myList.appendChild(title);
-    myList.appendChild(price);
-    myList.appendChild(btn);
-  }
-}
-
-customElements.define('product-item', ProductItem);// product-item.js
-
-class ProductItem extends HTMLElement {
-  // TODO
-  constructor() {
-    super();
-    let shadow = this.attachShadow({ mode: "open" });
-
-    let image = document.createElement("img");
-
-    let title = document.createElement("p");
-    title.setAttribute("class", "title");
-
-    let price = document.createElement("p");
-    price.setAttribute("class", "price");
-
     let button = document.createElement("button");
+
+    title.setAttribute("class", "title");
+    price.setAttribute("class", "price");
+    
     button.innerHTML = "Add to Cart";
     button.onclick = () => {
       let cart = localStorage.getItem("cart").split(",");
-      if (cart[this.id - 1] === "0") {
+      if (cart[this.id - 1] == "0") { 
         cart[this.id - 1] = "1";
         localStorage.setItem("cart", cart);
-
         button.innerHTML = "Remove from Cart";
         document.getElementById("cart-count").innerHTML++;
-      } else {
+      }  else {
         cart[this.id - 1] = "0";
         localStorage.setItem("cart", cart);
 
