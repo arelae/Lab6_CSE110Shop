01ed62ac72ddb1e7f3d2618c5c4c33f18b63564d
Jon Moon <44587416+jonjhmoon@users.noreply.github.com>
2021-02-13 17:01:56 -0800

    script

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,43 @@
 window.addEventListener("DOMContentLoaded", () => {
   let prods = JSON.parse(localStorage.getItem("products"));
 
-  if (prods === null) {
-    console.log("initial fetch");
+  // products are not initizalized
+  if (prods == null) {
+    // fetch json
     fetch("https://fakestoreapi.com/products")
-      .then((response) => response.json())
-      .then((data) => JSON.stringify(data))
-      .then((info) => localStorage.setItem("products", info))
-      .then(() => {
+      .then( (response) => response.json())
+      .then( (data) => JSON.stringify(data))
+      .then( (info) => localStorage.setItem("products", info))
+      .then( () => {
         prods = JSON.parse(localStorage.getItem("products"));
       })
-      .then(() => {
-        localStorage.setItem(
-          "cart",
-          new Array(prods.length).fill(0).toString()
-        );
-        for (const item in prods) loadProduct(prods[item]);
+      // populate localStorage
+      .then( () => {
+        localStorage.setItem("cart", new Array(prods.length).fill(0).toString());
+        for (const item in prods) loadProd(prods[item]);
       });
-  } else {
-    for (const item in prods) loadProduct(prods[item]);
+  } else { // products do exist
+    for (const item in prods) loadProd(prods[item]);
   }
 });
 
-let loadProduct = (item) => {
+// populating localStorage method
+let loadProd = (item) => {
   let cart = localStorage.getItem("cart").split(",");
   let list = document.createElement("product-item");
+  
   list.setAttribute("id", item.id);
+  
   document.getElementById("product-list").appendChild(list);
+
+  // sets shadowRoots
   list.shadowRoot.querySelector(".title").innerHTML = item.title;
-  list.shadowRoot.querySelector(".price").innerHTML =
-    "$" + item.price.toFixed(2);
+  list.shadowRoot.querySelector(".price").innerHTML = "$" + item.price.toFixed(2);
   list.shadowRoot.querySelector("img").src = item.image;
   list.shadowRoot.querySelector("img").src = item.image;
   list.shadowRoot.querySelector("img").alt = item.title;
 
-  if (cart[item.id - 1] === "0") {
+  if (cart[item.id - 1] == "0") {
     list.shadowRoot.querySelector("button").innerHTML = "Add to Cart";
   } else {
     list.shadowRoot.querySelector("button").innerHTML = "Remove from Cart";
